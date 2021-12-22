a37107c903d629bfe3df82846aebf9b20756f6fc
Mithun Roy <mithunroy25@gmail.com>
2021-12-23 01:43:59 +0400

    added basic home ui

---
+++ app/src/main/java/com/inhouse/classifiedsapp/ui/fragment/HomeFragment.kt
@@ -0,0 +1,21 @@
+package com.inhouse.classifiedsapp.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.inhouse.classifiedsapp.R
+import com.inhouse.classifiedsapp.viewmodel.HomeViewModel
+
+class HomeFragment : Fragment() {
+    private val homeViewModel: HomeViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.home_fragment, container, false)
+    }
+}
---
+++ app/src/main/java/com/inhouse/classifiedsapp/viewmodel/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.inhouse.classifiedsapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
